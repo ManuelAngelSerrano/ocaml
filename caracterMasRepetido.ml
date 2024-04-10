3083c2f241603622162f06ab3e53a926c9ef0801
Manuel Serrano <Manuel.Serrano@uclm.es>
2024-04-10 16:53:19 +0200

    Update caracterMasRepetido.ml

--- caracterMasRepetido.ml
+++ caracterMasRepetido.ml
@@ -9,26 +9,36 @@ let ordenar lista =
   (* Usamos la función compare como ordenación estándar *)
   List.fast_sort (compare) lista;;
 
-(* let rec agrupar lista =
+let rec agrupar' (lista: char list) (c:char) (acc:int) (max:char*int) : (char*int) = 
+(* let rec agrupar' : char list -> char -> int -> (char,int) : (char, int) = 
+  fun lista c acc max -> *)
   match lista with
-  | [] -> []
+  | [] -> max
   | x::xs -> 
-    let rec agrupar_aux lista x = 
-      match lista with
-      | [] -> []
-      | y::ys -> 
-        if x = y then
-          y::(agrupar_aux ys x)
-        else
-          []
-    in
-    let agrupado = agrupar_aux xs x in
-    if agrupado = [] then
-      x::(agrupar xs)
+    if x = c then
+      (agrupar' xs x acc (c, acc+1))
     else
-      x::agrupado::(agrupar xs);; *)
+      if xs =[] then
+        max
+      else
+        let h = List.hd xs in
+        let t = List.tl xs in
+        if acc > (snd max) then 
+          agrupar' t h 1 (h, acc)
+        else
+          agrupar' t h 1 max;;
 
-"En un lugar de la Mancha de cuyo nombre no quiero acordarme"
-|> str2list 
-|> ordenar
-|> List.iter (printf "%c ");;
+let maximo (lista: char list) : (char*int) = 
+  match lista with
+  | [] -> (' ', 0)
+  | x::xs -> agrupar' xs x 1 (x,1);;
+
+let masrepe (cadena: string) : (char*int)= 
+  cadena
+  |> str2list
+  |> ordenar
+  |> maximo;;
+
+
+let r = masrepe "En un lugar de la Mancha de cuyo nombre no quiero acordarme" in
+printf "(%c %d)" (fst r) (snd r);; 
