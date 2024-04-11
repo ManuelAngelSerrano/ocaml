197aed48c11175064df6c91386eed069b2b1c46b
Manuel Serrano <Manuel.Serrano@uclm.es>
2024-04-11 12:12:43 +0200

    caracter mas repetido

--- caracterMasRepetido.ml
+++ caracterMasRepetido.ml
@@ -2,36 +2,33 @@ open Printf;;
 
 let str2list s = 
   (* str2list con funciones estandar de OCaml *)
-  List.init (String.length s) (String.get s);;
+  (* Convertimos a lista de caracteres y quitamos (filter) los espacios en blanco *)
+  List.init (String.length s) (String.get s)
+  |> List.filter (fun x -> x != ' ');;
 
 let ordenar lista = 
   (* ordenar con funciones estandar de OCaml *)
   (* Usamos la función compare como ordenación estándar *)
   List.fast_sort (compare) lista;;
 
-let rec agrupar' (lista: char list) (c:char) (acc:int) (max:char*int) : (char*int) = 
+let rec agrupar' (lista: char list) (acc:char*int) (max:char*int) : (char*int) = 
 (* let rec agrupar' : char list -> char -> int -> (char,int) : (char, int) = 
   fun lista c acc max -> *)
   match lista with
   | [] -> max
   | x::xs -> 
-    if x = c then
-      (agrupar' xs x acc (c, acc+1))
+    if x = fst acc then
+      (agrupar' xs (x, (snd acc)+1) max)
     else
-      if xs =[] then
-        max
+      if (snd acc) > (snd max) then
+        agrupar' xs (x, 1) acc
       else
-        let h = List.hd xs in
-        let t = List.tl xs in
-        if acc > (snd max) then 
-          agrupar' t h 1 (h, acc)
-        else
-          agrupar' t h 1 max;;
+        agrupar' xs (x, 1) max;;
 
 let maximo (lista: char list) : (char*int) = 
   match lista with
   | [] -> (' ', 0)
-  | x::xs -> agrupar' xs x 1 (x,1);;
+  | x::xs -> agrupar' xs (x,1) (x,1);;
 
 let masrepe (cadena: string) : (char*int)= 
   cadena
@@ -40,5 +37,5 @@ let masrepe (cadena: string) : (char*int)=
   |> maximo;;
 
 
-let r = masrepe "En un lugar de la Mancha de cuyo nombre no quiero acordarme" in
-printf "(%c %d)" (fst r) (snd r);; 
+let res = masrepe "En un lugar de la Mancha de cuyo nombre no quiero acordarme" in
+printf "(%c %d)" (fst res) (snd res);; 
